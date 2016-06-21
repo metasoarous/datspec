e5d9201671e87f731ee6400dc7d59623c9f3dd4e
Christopher T Small <metasoarous@gmail.com>
2016-06-21 14:31:45 -0700

    Documentation

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# DatProtocols
+# Datspec
 
-Protocols for the DatSys architecture.
+Protocols and specs for the Datsys architecture.
 
-See `src/dat/protocols.cljc` for documentation.
+In a sense this _is_ the architecture.
+The rest of the Dat\* projects are really just implementations.
 
 
+## Overview
+
+This project provides a namespace `src/dat/protocols.cljc` of protocols
+which should be satisfied by system components (a la Stuart Sierra) making up an implementation of
+the abstract Datsys architecture.
+This modular design leaves a wide birth of flexibility for customization.
+
+
+## TODO
+
+More detailed documentation is coming soon.
+
+
+## Authors
+
+Christopher Small
+
+See LICENSE file for license.
 
 
--- project.clj
+++ project.clj
@@ -1,34 +1,15 @@
-(defproject datview "0.0.1-alpha1-SNAPSHOT"
-  :description "Effortlessly compose data visualizations and controls for Datomic and DataScript data"
-  :url "http://github.com/metasoarous/datview"
+(defproject datspec "0.0.1-alpha1-SNAPSHOT"
+  :description "Protocols and specs for the Datsys architecture - the shape of things."
+  :url "http://github.com/metasoarous/datspec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-alpha6"]
-                 [org.clojure/clojurescript "1.9.36"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
-                 ;; Datsys things
-                 [datspec "0.0.1-alpha1"]
-                 [datreactor "0.0.1-alpha1"]
-                 ;; Other stuff
-                 [com.stuartsierra/component "0.3.1"]
-                 [datascript "0.13.3"]
-                 [posh "0.3.3.1"]
-                 [reagent "0.5.1"]
-                 [io.rkn/conformity "0.4.0"] ;; should this be here?
-                 [prismatic/plumbing "0.5.2"]]
-  ;;
-  ;; ## Snipped from DataScript's
-  ;; ============================
-  ;;
-  ;; The following was taken from DataScript's project.clj; may need to clean up a bit
-  ;;
-  ;; Leaving this out for now
-  ;:global-vars {*warn-on-reflection* true}
+                 [org.clojure/clojurescript "1.9.36"]]
   :cljsbuild {:builds [{:id "release"
                         :source-paths ["src" "bench/src"]
                         :assert false
-                        :compiler {:output-to     "release-js/datsync.bare.js"
+                        :compiler {:output-to     "release-js/datspec.bare.js"
                                    :optimizations :advanced
                                    :pretty-print  false
                                    :elide-asserts true
@@ -40,24 +21,24 @@
                              [lein-typed "0.3.5"]]
                    :cljsbuild {:builds [{:id "advanced"
                                          :source-paths ["src" "bench/src" "test"]
-                                         :compiler {:output-to     "target/datsync.js"
+                                         :compiler {:output-to     "target/datspec.js"
                                                     :optimizations :advanced
-                                                    :source-map    "target/datsync.js.map"
+                                                    :source-map    "target/datspec.js.map"
                                                     :pretty-print  true
                                                     :recompile-dependents false
                                                     :parallel-build true}}
                                         {:id "none"
                                          :source-paths ["src" "bench/src" "test" "dev"]
-                                         :compiler {:main          datsync.test
-                                                    :output-to     "target/datsync.js"
+                                         :compiler {:main          datspec.test
+                                                    :output-to     "target/datspec.js"
                                                     :output-dir    "target/none"
                                                     :optimizations :none
                                                     :source-map    true
                                                     :recompile-dependents false
                                                     :parallel-build true}}]}}}
   :clean-targets ^{:protect false} ["target"
-                                    "release-js/datsync.bare.js"
-                                    "release-js/datsync.js"]
+                                    "release-js/datspec.bare.js"
+                                    "release-js/datspec.js"]
   ;;
   ;; ## Back to from extraction...
   ;; =============================
@@ -65,12 +46,11 @@
   ;; Once we're ready 
   ;:core.typed {:check []
                ;:check-cljs []}
-  ;;
-  ;; Not sure if we need these either
-  :resource-paths ["resources" "resources-index/prod"]
+  ;; Don't need resources for now
+  ;; Thought could maybe use this as a place for common schema code...
+  ;:resource-paths ["resources" "resources-index/prod"]
   :target-path "target/%s"
   :aliases {"package"
             ["with-profile" "prod" "do"
              "clean" ["cljsbuild" "once"]]})
-
 
